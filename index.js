d88002db0c39d1bde1a479fc28b85197e047fb26
Pablo Nafarrate <pablo_nafa@hotmail.com>
2018-12-05 23:30:24 -0700

    puerto

--- index.js
+++ index.js
@@ -2,10 +2,12 @@
 var app = require('./app');
 const mongoose = require('mongoose');
 
+app.set('port', process.env.PORT || 3000);
+
 mongoose.connect('mongodb://uscxbspmaxq7nya:puuWsySjy6lY9NkfPqB1@b84e4r8r3dcrwrs-mongodb.services.clever-cloud.com:27017/b84e4r8r3dcrwrs', { useNewUrlParser: true }, (err) => {
     if (!err) {
-        app.listen(3000, () => {
-            console.log('El servidor corre en el http://localhost:3000');
+        app.listen(app.get('port'), () => {
+            console.log(`El servidor corre en el http://localhost:${app.get('port')}`);
         });
     }
 });
